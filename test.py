71967bfd6b7342f43109f89b30bddcb7c4dd7efe
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-02-14 11:27:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ dino = Actor("dino1")
 cactus = Actor("cactus")
 
 dino.pos = (dino.width, 160)
-cactus.pos = (WIDTH-cactus.width, 170)
+cactus.pos = (WIDTH - cactus.width, 170)
 
 state = "pause"
-jumping = False
-jump_speed = 0
+jumping = True
+jump_speed = 13
 
 def draw():
     screen.clear()
@@ -22,24 +22,36 @@ def draw():
 
 def on_key_down(key):
     global state, jumping, jump_speed
-    if key == keys.SPACE and state == "pause":
+    if key == keys.SPACE and state != 'running':
         state = "running"
-    elif key == keys.SPACE and not jumping:
-        jumping = True
+        dino.pos = (dino.width, 160)
+        jumping = False
+        jump_speed = 0
+        dino.image = "dino1"
+        cactus.pos = (WIDTH - cactus.width, 170)
+
+    if key == keys.SPACE and not jumping:
         jump_speed = 13
-    
+        jumping = True
+
 def update(dt):
-    global state, jumping, jump_speed
-    if state == "running":
+    global jumping, state, jump_speed
+
+    if state == 'running':
+        cactus.x -= 10
+        if cactus.x < 0:
+            cactus.x = WIDTH
+
+        if dino.colliderect(cactus) and abs(cactus.center[0] - dino.center[0]) < 30:
+            state = 'pause'
+            dino.pos = (WIDTH // 2, HEIGHT // 2)
+            dino.image = "restart" 
+
         if jumping:
             dino.y -= jump_speed
-            jump_speed -=1
-            if jump_speed <= -13:
+            jump_speed -= 1
+            if jump_speed < -13:
                 dino.y = 160
                 jump_speed = 0
                 jumping = False
-
-
-
-
-
+    
