6ca73e9527625aab68a2dce437ce463cb5d58521
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-02-21 11:23:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,57 +1,48 @@
 import pgzero
+import random
 
-WIDTH = 800
-HEIGHT = 200
-FPS = 30
+WIDTH = 288    
+HEIGHT = 512   
+FPS = 30      
 
-dino = Actor("dino1")
-cactus = Actor("cactus")
+bg = Actor("bg2")      
+bird = Actor("bird1")
+pipe_top = Actor("top")
+pipe_bottom = Actor("bottom")
+bird.x = 50            
+bird.y = HEIGHT//2
 
-dino.pos = (dino.width, 160)
-cactus.pos = (WIDTH - cactus.width, 170)
+velocity_y = 0
+pipe_speed = 3
 
-state = "pause"
-jumping = True
-jump_speed = 13
-
-def draw():
-    screen.clear()
-    screen.fill((255, 255, 255))
-    dino.draw()
-    cactus.draw()
+def reset_pipes():
+    gap_y = random.randint(200, HEIGHT - 150)
+    pipe_top.pos = (WIDTH + 50, gap_y - pipe_top.height // 2 - 100)  
+    pipe_bottom.pos = (WIDTH + 50, gap_y + pipe_top.height // 2)  
+reset_pipes()
 
 def on_key_down(key):
-    global state, jumping, jump_speed
-    if key == keys.SPACE and state != 'running':
-        state = "running"
-        dino.pos = (dino.width, 160)
-        jumping = False
-        jump_speed = 0
-        dino.image = "dino1"
-        cactus.pos = (WIDTH - cactus.width, 170)
-
-    elif key == keys.SPACE and not jumping:
-        jump_speed = 13
-        jumping = True
+    global velocity_y
+    if key == keys.SPACE:
+        velocity_y -= 8
 
 def update(dt):
-    global jumping, state, jump_speed
+    global velocity_y, pipe_top, pipe_bottom, pipe_speed
+    if pipe_speed > 0:
+        bird.y += velocity_y
+        velocity_y += 0.8
+        pipe_top.x -= pipe_speed
+        pipe_bottom.x -= pipe_speed
 
-    if state == 'running':
-        cactus.x -= 10
-        if cactus.x < 0:
-            cactus.x = WIDTH
+        if pipe_top.x <= -50:
+            reset_pipes()
+        if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom) or bird.y < 0 or bird.y > HEIGHT:
+            pipe_speed = 0
 
-        if dino.colliderect(cactus) and abs(cactus.center[0] - dino.center[0]) < 30:
-            state = 'pause'
-            dino.pos = (WIDTH // 2, HEIGHT // 2)
-            dino.image = "restart" 
+def draw():        
+    screen.clear() 
+    bg.draw()      
+    bird.draw()
+    pipe_top.draw()
+    pipe_bottom.draw()
 
-        if jumping:
-            dino.y -= jump_speed
-            jump_speed -= 1
-            if jump_speed < -13:
-                dino.y = 160
-                jump_speed = 0
-                jumping = False
-    
