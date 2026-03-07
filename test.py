33a7a2a231331f4fff39b22a35816565fdfcc9eb
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-03-07 10:32:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,55 +1,21 @@
 import pgzero
-import random
-
-WIDTH = 288    
-HEIGHT = 512   
-FPS = 30      
-
-bg = Actor("bg2")      
-bird = Actor("bird1")
-pipe_top = Actor("top")
-pipe_bottom = Actor("bottom")
-gameover = Actor("gameover")
-gameover.y = HEIGHT//2
-gameover.x = WIDTH//2
-bird.x = 50            
-bird.y = HEIGHT//2
-
-velocity_y = 0
-pipe_speed = 3
-
-def reset_pipes():
-    gap_y = random.randint(200, HEIGHT - 150)
-    pipe_top.pos = (WIDTH + 50, gap_y - pipe_top.height // 2 - 100)  
-    pipe_bottom.pos = (WIDTH + 50, gap_y + pipe_top.height // 2)  
-reset_pipes()
-
-def on_key_down(key):
-    global velocity_y
-    if key == keys.SPACE:
-        velocity_y -= 8
-
-def update(dt):
-    global velocity_y, pipe_top, pipe_bottom, pipe_speed
-    if pipe_speed > 0:
-        bird.y += velocity_y
-        velocity_y += 0.8
-        pipe_top.x -= pipe_speed
-        pipe_bottom.x -= pipe_speed
-
-        if pipe_top.x <= -50:
-            reset_pipes()
-        if bird.colliderect(pipe_top) or bird.colliderect(pipe_bottom) or bird.y < 0 or bird.y > HEIGHT:
-            pipe_speed = 0
-
-
+#КОНСТАНТЫ
+WIDTH=600
+HEIGHT=600
+#ПЕРЕМЕННЫЕ
+cookies = 0
+#ПЕРСОНАЖИ
+bg = Actor("bgBlue")
+cookie = Actor("cookie", (WIDTH//2, HEIGHT//2))
+#ФУНКЦИЯ ОТРИСОВКИ
 def draw():
-    global gameover  
-    screen.clear() 
-    bg.draw()      
-    bird.draw()
-    pipe_top.draw()
-    pipe_bottom.draw()
-    if pipe_speed == 0:
-        gameover.draw()
-
+    screen.clear()
+    bg.draw()
+    cookie.draw()
+    screen.draw.text(str(cookies), 
+                center=(WIDTH//2,  50), 
+                color=(255, 153, 51), 
+                fontsize=50)
+#ОСНОВНОЙ ИГРОВОЙ ЦИКЛ
+def update(dt):
+    cookie.angle += 1
