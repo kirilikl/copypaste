c896da3c6b246dbd9543915d6326f3402448bdfb
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-03-07 11:25:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,24 @@ HEIGHT=600
 #ПЕРЕМЕННЫЕ
 cookies = 0
 points_pos = []
+autoclick = 0
 #ПЕРСОНАЖИ
 bg = Actor("bgBlue")
 cookie = Actor("cookie", (WIDTH//2, HEIGHT//2))
+cursor = Actor("cursor", (50, HEIGHT//2))
+#СВОИ ФУНКЦИИ
+def autoclickFunc():
+    global cookies
+    cookies += autoclick
+
+#ТАЙМЕР
+clock.schedule_interval(autoclickFunc, 1)
 #ФУНКЦИЯ ОТРИСОВКИ
 def draw():
     screen.clear()
     bg.draw()
     cookie.draw()
+    cursor.draw()
     screen.draw.text(str(cookies), 
                 center=(WIDTH//2,  50), 
                 color=(255, 153, 51), 
@@ -33,7 +43,10 @@ def update(dt):
             points_pos.remove(i)
 #ОБРАБОТЧИК МЫШИ
 def on_mouse_down(button, pos):
-    global cookies
+    global cookies, autoclick
     if button == mouse.LEFT and cookie.collidepoint(pos):
        points_pos.append(pos)
        cookies += 1
+    if button == mouse.LEFT and cursor.collidepoint(pos):
+        autoclick += 1
+
