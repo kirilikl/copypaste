aa503ff5dd5e4ec8cd31e47ec42737a1ab9f0667
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-03-07 11:07:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ WIDTH=600
 HEIGHT=600
 #ПЕРЕМЕННЫЕ
 cookies = 0
+points_pos = []
 #ПЕРСОНАЖИ
 bg = Actor("bgBlue")
 cookie = Actor("cookie", (WIDTH//2, HEIGHT//2))
@@ -16,6 +17,12 @@ def draw():
                 center=(WIDTH//2,  50), 
                 color=(255, 153, 51), 
                 fontsize=50)
+    for i in points_pos:
+        screen.draw.text("+1", 
+                center=i, 
+                color=(255, 255, 255), 
+                fontsize=30)
+
 #ОСНОВНОЙ ИГРОВОЙ ЦИКЛ
 def update(dt):
     cookie.angle += 1
@@ -23,10 +30,5 @@ def update(dt):
 def on_mouse_down(button, pos):
     global cookies
     if button == mouse.LEFT and cookie.collidepoint(pos):
-        cookies += 1
-
-
-
-
-
-
+       points_pos.append(pos)
+       cookies += 1
