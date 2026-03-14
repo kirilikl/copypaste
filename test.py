897cc608a7273d3587b6217c396128f098ed699e
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-03-14 10:26:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,17 @@ HEIGHT=600
 #ПЕРЕМЕННЫЕ
 cookies = 0
 points_pos = []
-autoclick = 0
 #ПЕРСОНАЖИ
 bg = Actor("bgBlue")
 cookie = Actor("cookie", (WIDTH//2, HEIGHT//2))
 cursor = Actor("cursor", (50, HEIGHT//2))
+cursor.value = 1
+cursor.price = 10
+cursor.amount = 0
 #СВОИ ФУНКЦИИ
 def autoclickFunc():
     global cookies
-    cookies += autoclick
+    cookies += cursor.value * cursor.amount
 
 #ТАЙМЕР
 clock.schedule_interval(autoclickFunc, 1)
@@ -23,16 +25,10 @@ def draw():
     bg.draw()
     cookie.draw()
     cursor.draw()
-    screen.draw.text(str(cookies), 
-                center=(WIDTH//2,  50), 
-                color=(255, 153, 51), 
-                fontsize=50)
+    screen.draw.text(str(cursor.price), center=(cursor.x, cursor.y+50), color=(255, 255, 255), fontsize=25)
+    screen.draw.text(str(cookies), center=(WIDTH//2,  50), color=(255, 153, 51), fontsize=50)
     for i in points_pos:
-        screen.draw.text("+1", 
-                center=i, 
-                color=(255, 255, 255), 
-                fontsize=30)
-
+        screen.draw.text("+1", center=i, color=(255, 255, 255), fontsize=30)
 #ОСНОВНОЙ ИГРОВОЙ ЦИКЛ
 def update(dt):
     cookie.angle += 1
@@ -49,4 +45,3 @@ def on_mouse_down(button, pos):
        cookies += 1
     if button == mouse.LEFT and cursor.collidepoint(pos):
         autoclick += 1
-
