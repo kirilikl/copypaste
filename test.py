8508ff2b73779d5a88131dde67bf698f99448e2d
kirilikl <81564074+kirilikl@users.noreply.github.com>
2026-03-21 11:16:10 +0300

    Add autoclick functionality and upgrade handling

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ HEIGHT=600
 #ПЕРЕМЕННЫЕ
 cookies = 0
 points_pos = []
+autoclickPerSecond = 0
 #ПЕРСОНАЖИ
 bg = Actor("bgBlue")
 cookie = Actor("cookie", (WIDTH//2, HEIGHT//2))
@@ -24,13 +25,14 @@ garden.value = 10
 garden.price = 500
 garden.amount = 0
 
+upgrades = [cursor, grandma, garden]
 #СВОИ ФУНКЦИИ
 def autoclickFunc():
-    global cookies
-    cookies += cursor.value * cursor.amount
-    cookies += grandma.value * grandma.amount
-    cookies += garden.value * garden.amount
- 
+    global cookies, upgrades
+    autoclickPerSecond = 0
+    for i in upgrades:
+        autoclickPerSecond += i.value * i.amount
+        cookies += autoclickPerSecond
 #ТАЙМЕР
 clock.schedule_interval(autoclickFunc, 1)
 #ФУНКЦИЯ ОТРИСОВКИ
@@ -39,42 +41,37 @@ def draw():
     bg.draw()
     cookie.draw()
     screen.draw.text(str(cookies), center=(WIDTH//2,  50), color=(255, 153, 51), fontsize=50)
+    screen.draw.text('+' + str(autoclickPerSecond), center=(WIDTH//2,  95), color=(0, 255, 255), fontsize=25)
+    
     for i in points_pos:
-        screen.draw.text("+1", center=i, color=(255, 255, 255), fontsize=30)
+        screen.draw.text("+1", center=i, color=(255 , 255, 255), fontsize=30)
+    for i in upgrades:
+        i.draw()
+        if cookies >= i.price:
+            screen.draw.text(str(i.price), center=(i.x, i.y+50), color=(100, 255, 100), fontsize=25)
+        else:
+            screen.draw.text(str(i.price), center=(i.x, i.y+50), color=(255, 100, 100), fontsize=25)
 
-    cursor.draw()
-    screen.draw.text(str(cursor.price), center=(cursor.x, cursor.y+50), color=(255, 255, 255), fontsize=25)
-    grandma.draw()
-    screen.draw.text(str(grandma.price), center=(grandma.x, grandma.y+50), color=(255, 255, 255), fontsize=25)
-    garden.draw()
-    screen.draw.text(str(garden.price), center=(garden.x, garden.y+50), color=(255, 255, 255), fontsize=25)
+    
 #ОСНОВНОЙ ИГРОВОЙ ЦИКЛ
 def update(dt):
     cookie.angle += 1
     for i in points_pos:
-        if i[1] > 0:
+        if i[1] > 0:    
             i[1] -=4
         else:
             points_pos.remove(i)
+            
 #ОБРАБОТЧИК МЫШИ
 def on_mouse_down(button, pos):
     global cookies, autoclick
     if button == mouse.LEFT and cookie.collidepoint(pos):
        points_pos.append(pos)
        cookies += 1
-    if button == mouse.LEFT and cursor.collidepoint(pos):
-        if cookies >= cursor.price:
-            cursor.amount += 1
-            cookies -= cursor.price
-            cursor.price = int(cursor.price * 1.5)
-    if button == mouse.LEFT and grandma.collidepoint(pos):
-        if cookies >= grandma.price:
-            grandma.amount += 1
-            cookies -= grandma.price
-            grandma.price = int(grandma.price * 1.5)
-    if button == mouse.LEFT and garden.collidepoint(pos):
-        if cookies >= garden.price:
-            garden.amount += 1
-            cookies -= garden.price
-            garden.price = int(garden.price * 1.5)
- 
+    for i in upgrades:
+        if button == mouse.LEFT and i.collidepoint(pos):
+            if cookies >= i.price:
+                i.amount += 1
+                cookies -= i.price
+                i.price = int(i.price * 1.5)
+    
